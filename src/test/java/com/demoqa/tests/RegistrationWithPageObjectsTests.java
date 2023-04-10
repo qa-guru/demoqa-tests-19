0c2378470b70fcaf3958e1b0f199102100e053fd
svasenkov <aanher@gmail.com>
2023-04-10 21:14:11 +0200

    added more pageobjects

--- src/test/java/com/demoqa/tests/RegistrationWithPageObjectsTests.java
+++ src/test/java/com/demoqa/tests/RegistrationWithPageObjectsTests.java
@@ -2,8 +2,6 @@ package com.demoqa.tests;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
