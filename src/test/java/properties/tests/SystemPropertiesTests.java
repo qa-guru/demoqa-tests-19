645652ce438c6ea11f61cc00f5006099956bf5db
svasenkov <aanher@gmail.com>
2023-05-04 20:20:53 +0200

    added hello test

--- src/test/java/properties/tests/SystemPropertiesTests.java
+++ src/test/java/properties/tests/SystemPropertiesTests.java
@@ -3,6 +3,8 @@ package properties.tests;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.String.format;
+
 public class SystemPropertiesTests {
 
     @Test
@@ -46,6 +48,25 @@ public class SystemPropertiesTests {
 
         // gradle property_test -Dbrowser=opera
         // opera
+
+    }
+
+    @Test
+    @Tag("hello")
+    void systemProperties5Test() {
+        String name = System.getProperty("name", "default student");
+        String message = format("Hello, %s!", name);
+
+        System.out.println(message);
+        // gradle hello_test
+        // Hello, default student!
+
+        // gradle hello_test -Dname=Alex Egorov
+        // BUILD FAILED: Task 'Egorov' not found in root project 'demoqa-tests-19'.
+
+        // gradle hello_test -Dname="Alex Egorov"
+        // gradle hello_test "-Dname=Alex Egorov"
+        // Hello, Alex Egorov!
 
     }
 }
